7a952be1252d1e16a8c8dd3c1db2b3158430c7e5
faizullahkhan8 <faizullahofficial0@gmail.com>
2024-11-07 08:22:13 -0800

    change from react.js to next.ts

--- frontend/src/Routes/user.router.jsx
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-
-export const userRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <h1>this is home</h1>,
-    },
-]);
